7a817d55fdbe7226433e2f1f32ee636d7bc80938
Riad Kanane <one@kanane.com>
2025-03-06 22:01:27 +0000

    TW config

--- themes/dreamiplaycafe/tailwind.config.js
+++ themes/dreamiplaycafe/tailwind.config.js
@@ -1,51 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.php",
-    "./page-templates/**/*.php",
-    "./template-parts/**/*.php",
-    "./js/**/*.js",
-    "./inc/**/*.php",
-    "./templates/**/*.php",
-    "./woocommerce/**/*.php",
-  ],
-  safelist: [
-    "bg-gradient-to-r",
-    "from-purple-500",
-    "to-pink-500",
-    "p-8",
-    "container",
-    "mx-auto",
-    "shadow-xl",
-    "hover:scale-105",
-    "transition-transform",
-    "duration-300",
-    "grid",
-    "grid-cols-1",
-    "md:grid-cols-3",
-    "gap-4",
-    "rounded-lg",
-    "hover:bg-blue-200",
-    "hover:bg-green-200",
-    "hover:bg-yellow-200",
-    "text-gray-600",
-    "text-gray-800",
-    "text-3xl",
-    "text-xl",
-    "font-bold",
-    "font-semibold",
+    './*.php',
+    './page-templates/*.php',
+    './template-parts/*.php',
+    './js/*.js',
+    './inc/*.php',
+    './templates/*.php',
+    './woocommerce/*.php',
+    './woocommerce/**/*.php'
   ],
   theme: {
     extend: {
       colors: {
-        primary: "var(--wp--preset--color--primary)",
-        secondary: "var(--wp--preset--color--secondary)",
+        'primary': 'var(--wp--preset--color--primary)',
+        'secondary': 'var(--wp--preset--color--secondary)',
       },
       container: {
         center: true,
-        padding: "1rem",
+        padding: '1rem',
       },
     },
   },
   plugins: [],
-};
+} 
