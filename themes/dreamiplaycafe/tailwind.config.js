a3abdbc1a3c2555edf619fa06f9a2531a1e5f562
Riad Kanane <one@kanane.com>
2025-03-06 21:37:09 +0000

    minor tweak

--- themes/dreamiplaycafe/tailwind.config.js
+++ themes/dreamiplaycafe/tailwind.config.js
@@ -1,26 +1,36 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './*.php',
-    './page-templates/*.php',
-    './template-parts/*.php',
-    './js/*.js',
-    './inc/*.php',
-    './templates/*.php',
-    './woocommerce/*.php',
-    './woocommerce/**/*.php'
+    "./*.php",
+    "./page-templates/*.php",
+    "./template-parts/*.php",
+    "./js/*.js",
+    "./inc/*.php",
+    "./templates/*.php",
+    "./woocommerce/**/*.php",
+  ],
+  safelist: [
+    "bg-gradient-to-r",
+    "from-purple-500",
+    "to-pink-500",
+    "p-8",
+    "container",
+    "mx-auto",
+    "shadow-xl",
+    "hover:scale-105",
+    "transition-transform",
+    "duration-300",
   ],
   theme: {
     extend: {
       colors: {
-        'primary': 'var(--wp--preset--color--primary)',
-        'secondary': 'var(--wp--preset--color--secondary)',
+        primary: "var(--wp--preset--color--primary)",
+        secondary: "var(--wp--preset--color--secondary)",
       },
       container: {
         center: true,
-        padding: '1rem',
+        padding: "1rem",
       },
     },
   },
   plugins: [],
-} 
+};
