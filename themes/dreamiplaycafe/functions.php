f36df3b1aa5ee84a1df40adadc38b5ad3f5c8ac4
Riad Kanane <one@kanane.com>
2025-03-06 20:39:53 +0000

    Added unecessary script loader for WP

--- themes/dreamiplaycafe/functions.php
+++ themes/dreamiplaycafe/functions.php
@@ -176,3 +176,27 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Disable WordPress emoji scripts and styles
+ */
+function dreamiplaycafe_disable_emojis() {
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+}
+add_action('init', 'dreamiplaycafe_disable_emojis');
+
+/**
+ * Disable WordPress block library styles
+ */
+function dreamiplaycafe_disable_block_styles() {
+	wp_dequeue_style('wp-block-library');
+	wp_dequeue_style('wp-block-library-theme');
+	wp_dequeue_style('global-styles');
+}
+add_action('wp_enqueue_scripts', 'dreamiplaycafe_disable_block_styles', 100);
+
